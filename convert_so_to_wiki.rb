0fb7f8c108530ce36dc2e2314350d95e594d805a
paul.beyer <plbeyer70@gmail.com>
2020-04-07 12:05:17 -0400

    pulling all the stackoverflow data in, except images

--- convert_so_to_wiki.rb
+++ convert_so_to_wiki.rb
@@ -1,9 +1,12 @@
 require 'json'
 
-so-accounts = JSON.parse File.read './northwoods/accounts.json'
-so-badges = JSON.parse File.read './northwoods/badges.json'
-so-badges = JSON.parse File.read './northwoods/badges.json'
-so-badges = JSON.parse File.read './northwoods/badges.json'
-'testing an additional change'
+SO_ACCOUNTS = JSON.parse File.read '../stackoverflow/northwoods/accounts.json'
+SO_BADGES = JSON.parse File.read '../stackoverflow/northwoods/badges.json'
+SO_COMMENTS = JSON.parse File.read '../stackoverflow/northwoods/comments.json'
+SO_IMAGES = JSON.parse File.read '../stackoverflow/northwoods/images.json'
+SO_POSTS = JSON.parse File.read '../stackoverflow/northwoods/posts.json'
+SO_POSTS2VOTES = JSON.parse File.read '../stackoverflow/northwoods/posts2votes.json'
+SO_TAGS = JSON.parse File.read '../stackoverflow/northwoods/tags.json'
+SO_USERS = JSON.parse File.read '../stackoverflow/northwoods/users.json'
+SO_USERS2BADGES = JSON.parse File.read '../stackoverflow/northwoods/users2badges.json'
 
-puts accounts
