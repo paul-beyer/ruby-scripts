172d2ae725e097fcc2b5287fd77ca67484a2f80f
paul.beyer <plbeyer70@gmail.com>
2020-04-07 13:52:49 -0400

    writing title and markdown body to file

--- convert_so_to_wiki.rb
+++ convert_so_to_wiki.rb
@@ -10,3 +10,33 @@ SO_TAGS = JSON.parse File.read '../stackoverflow/northwoods/tags.json'
 SO_USERS = JSON.parse File.read '../stackoverflow/northwoods/users.json'
 SO_USERS2BADGES = JSON.parse File.read '../stackoverflow/northwoods/users2badges.json'
 
+@questions = []
+
+def get_questions
+    puts "Getting all questions"
+    SO_POSTS.each { |post| @questions << post if post["postType"] == "question" }
+    puts "Number of questions = #{@questions.length}"
+end
+
+def open_file
+    File.write("stackoverflow.md", "Stack Overflow Conversion to Markdown - #{Time.now} \n")
+end
+
+def write_to_file(text)
+    File.write("stackoverflow.md", text, mode: "a")
+end
+
+def extract_question_data
+    @questions.each do |question|
+        title = question["title"]
+        body = question["bodyMarkdown"]
+        write_to_file "# " + title + "\n\n"
+        write_to_file "#{body} \n\n"
+    end
+end
+
+open_file
+get_questions
+extract_question_data
+    
+
