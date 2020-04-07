3b9643a44a20a4ca5359bc93e1c52f69cd83a504
Paul Beyer <plbeyer70@gmail.com>
2020-04-07 10:16:40 -0400

    Initial commit

---
+++ convert_so_to_wiki.rb
@@ -0,0 +1,9 @@
+require 'json'
+
+so-accounts = JSON.parse File.read './northwoods/accounts.json'
+so-badges = JSON.parse File.read './northwoods/badges.json'
+so-badges = JSON.parse File.read './northwoods/badges.json'
+so-badges = JSON.parse File.read './northwoods/badges.json'
+
+
+puts accounts
