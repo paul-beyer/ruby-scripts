fb61c85eec94e47e61e6901c8064c5651a6c0ee0
paul.beyer <plbeyer70@gmail.com>
2020-04-08 12:11:38 -0400

    added comments to answers and questions

--- convert_so_to_wiki.rb
+++ convert_so_to_wiki.rb
@@ -10,13 +10,13 @@ SO_TAGS = JSON.parse File.read '../stackoverflow/northwoods/tags.json'
 SO_USERS = JSON.parse File.read '../stackoverflow/northwoods/users.json'
 SO_USERS2BADGES = JSON.parse File.read '../stackoverflow/northwoods/users2badges.json'
 
-@questions = []
-@answers = {} 
 
 def get_questions
+    questions = []
     puts "Getting all questions"
-    SO_POSTS.each { |post| @questions << post if post["postType"] == "question" }
-    puts "Number of questions = #{@questions.length}"
+    SO_POSTS.each { |post| questions << post if post["postType"] == "question" }
+    puts "Number of questions = #{questions.length}"
+    questions
 end
 
 def open_file
@@ -32,6 +32,11 @@ def get_user_name(answer)
     answer['ownerUserId'] > 0 ? SO_USERS.find { |user| user['id'] == answer['ownerUserId']}['realName'] : "Community User"
 end
 
+def get_comment_user_name(comment)
+    return comment['userDisplayName'] if comment['userDisplayName']
+    comment['userId'] > 0 ? SO_USERS.find { |user| user['id'] == comment['userId']}['realName'] : "Community User"
+end
+
 # not called yet
 def fix_markdown(text)
     fixed_text = text
@@ -40,30 +45,43 @@ def fix_markdown(text)
     fixed_text
 end
 
-def extract_question_data
-    @questions.each do |question|
+def write_comments(comments, post_id)
+    comments_for_post = comments[post_id]
+    write_to_file("### Comments\n\n") if comments_for_post
+    comments_for_post&.each {|comment| write_to_file(">#{comment['text']} - #{get_comment_user_name(comment)} - #{comment['creationDate']}\n\n") }
+end
+
+def extract_question_data(questions, answers, comments)
+    questions.each do |question|
         title = question["title"]
         body = question["bodyMarkdown"]
         write_to_file "\# #{title}\n\n"
         write_to_file "#{body} \n\n"
-        answers_for_question = @answers[question["id"]] || []
-        write_to_file "#{answers_for_question.length} #{answers_for_question.length == 1 ? 'Answer' : 'Answers'}\n\n" 
+        write_comments(comments, question['id']) 
+        answers_for_question = answers[question["id"]] || []
+        write_to_file "### #{answers_for_question.length} #{answers_for_question.length == 1 ? 'Answer' : 'Answers'}\n\n" 
         answers_for_question.each_with_index do |answer,index|
             userName = get_user_name(answer)
             write_to_file "Answer #{index+1} by #{userName} #{question['acceptedAnswerId'] == answer['id'] ? '(Accepted Answer)' : ''}\n\n"
-            write_to_file "#{answer['bodyMarkdown']}\n" 
+            write_to_file "#{answer['bodyMarkdown']}\n\n" 
+            write_comments(comments, answer['id'])
         end
     end
 end
 
 def group_answers_by_question 
-  all_answers = SO_POSTS.find_all { |post| post["postType"] == "answer" }
-  @answers = all_answers.group_by { |answer| answer["parentId"] }
+  all_answers = SO_POSTS.find_all { |post| post['postType'] == 'answer' }
+  all_answers.group_by { |answer| answer['parentId'] }
+end
+
+def group_comments_by_post 
+    SO_COMMENTS.group_by { |comment| comment['postId'] }
 end
 
 open_file
-get_questions
-group_answers_by_question 
-extract_question_data
+questions = get_questions
+answers = group_answers_by_question 
+comments = group_comments_by_post
+extract_question_data(questions, answers, comments)
     
 
